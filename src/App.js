34a6bdfb836d32d496b47d1b6bdac4e5dbe62e3d
Solomonchik <viktorzimmermann11@web.de>
2019-03-06 09:28:54 +0100

    started

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <div className="buttonsGroup">
+              <a href="">About</a>
+              <a href="">Contacts</a>
+          </div>
+      </div>
+    );
+  }
+}
+
+export default App;
